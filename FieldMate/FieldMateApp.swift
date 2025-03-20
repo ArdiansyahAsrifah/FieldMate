9c7beff777b42341f1093a1b8d9b346ab5c7104a
ArdiansyahAsrifah <ancakiky@gmail.com>
2025-03-20 19:15:16 +0700

    Initial Commit

---
+++ FieldMate/FieldMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  FieldMateApp.swift
+//  FieldMate
+//
+//  Created by Muhammad Ardiansyah Asrifah on 20/03/25.
+//
+
+import SwiftUI
+
+@main
+struct FieldMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FieldMateTests/FieldMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  FieldMateTests.swift
+//  FieldMateTests
+//
+//  Created by Muhammad Ardiansyah Asrifah on 20/03/25.
+//
+
+import Testing
+@testable import FieldMate
+
+struct FieldMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
